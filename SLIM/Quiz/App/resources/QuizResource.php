84fb396bf0dcab5539e025d1fd5cf222173ea8b5
eslam <elbadry693@gmail.com>
2024-07-31 22:30:18 +0300

    fix quiz error

--- SLIM/Quiz/App/resources/QuizResource.php
+++ SLIM/Quiz/App/resources/QuizResource.php
@@ -17,6 +17,8 @@ class QuizResource extends JsonResource
             'quiz_date' => $this->quiz_date,
             'is_complete' => (bool)$this->is_complete,
             'level' => $this->level,
+            'questions_count' => $this->list_questions_count,
+            'correct_answers_count' => $this->correct_answers_count,
             'result' => $this->list_questions_count ? ($this->correct_answers_count / $this->list_questions_count) * 100 : 0,
         ];
     }
