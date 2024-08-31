01d94814c8b88eb98d05c05cd53aad5274c539c3
eslam <elbadry693@gmail.com>
2024-08-31 14:50:31 +0300

    add delete account api and activate and not active payment method

--- SLIM/Package/App/Http/Controllers/Api/PackageController.php
+++ SLIM/Package/App/Http/Controllers/Api/PackageController.php
@@ -25,7 +25,7 @@ class PackageController extends Controller
         $packages = Package::query()
             ->where('is_active',1)
             ->with(['activeSubscribe' => fn($query) => $query->where('is_active',true)
-                ->where('status','!=',SubscribeStatusEnum::FINISHED->value)->where('trainee_id', auth()->id())])
+                ->where('subscribe_status','!=',SubscribeStatusEnum::FINISHED->value)->where('trainee_id', auth()->id())])
             ->get();
         $packages = PackageResource::collection($packages);
         return $this->returnData($packages, 'Packages List');
